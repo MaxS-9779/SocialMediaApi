ff943810afe22708c116104a138457da8069c69d
MaxS97 <maks_sazanov1998@mail.ru>
2025-06-20 18:22:11 +0300

    Initial commit

---
+++ src/test/java/restful/api/SocialMediaApi/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package restful.api.SocialMediaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
